2014d369b7201ea7bbe102bcd1b611861e320f79
Javier Ibanez <Expertprep2015@gmail.com>
2023-03-01 12:15:13 -0500

    fixed images with proper path

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,10 @@
+import ReactLogo from "../react-logo.png"
+
 export default function Header() {
     return (
         <header>
             <nav className='nav'>
-                <img className='nav-logo' src="testvite\react-logo.png" alt="reactlogo2sd"/>
+                <img className='nav-logo' src={ReactLogo} alt="reactlogo2sd"/>
                 <ul className='nav-items'>
                   <li>Pricing</li>
                   <li>About</li>
